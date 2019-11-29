938a452e45b7c36304c73a8c934a295080094f92
Carlo Lim <carlojameslim1@gmail.com>
2019-11-30 03:46:35 +0800

    setup next js and github pages

--- next.config.js
+++ next.config.js
@@ -1,2 +1,20 @@
 const withCSS = require('@zeit/next-css');
-module.exports = withCSS();
+const webpack = require('webpack')
+const isProd = (process.env.NODE_ENV || 'production') === 'production'
+const assetPrefix = isProd ? '/screen-recorder' : ''
+module.exports = withCSS({
+    exportPathMap: () => ({
+        '/': { page: '/' },
+        '/recorder': { page: '/recorder' },
+    }),
+    assetPrefix: assetPrefix,
+    webpack: config => {
+        config.plugins.push(
+            new webpack.DefinePlugin({
+                'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
+            }),
+        )
+
+        return config
+    },
+})
